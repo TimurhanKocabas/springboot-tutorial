e29aadcf7cc3ab2cf865176ddfe54507b26e080f
timurhan kocabas <timurhankocabas@outlook.com>
2023-12-30 20:55:45 +0100

    project init

---
+++ src/main/kotlin/dev/cpttimy/tutorials/springboot/captaintimmy/CaptaintimmyApplication.kt
@@ -0,0 +1,11 @@
+package dev.cpttimy.tutorials.springboot.captaintimmy
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CaptaintimmyApplication
+
+fun main(args: Array<String>) {
+	runApplication<CaptaintimmyApplication>(*args)
+}
---
+++ src/test/kotlin/dev/cpttimy/tutorials/springboot/captaintimmy/CaptaintimmyApplicationTests.kt
@@ -0,0 +1,13 @@
+package dev.cpttimy.tutorials.springboot.captaintimmy
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CaptaintimmyApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
